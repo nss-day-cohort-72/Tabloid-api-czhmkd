5d1d9b2dd61554a1f0fe59e258c7babdc027a0bf
Ben Merlotti <benmerlotti@Bens-Laptop.local>
2025-01-08 09:39:51 -0700

    added crud controllers for categories

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Tabloid.Models;
+using Tabloid.Models.DTOs;
 using Tabloid.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
@@ -38,9 +39,51 @@ public class CategoriesController : ControllerBase
         return Ok(categories);
     }
 
+    [HttpGet("{id}")]
+    [Authorize(Roles = "Admin")]
+    public IActionResult GetCategoryById(int id)
+    {
+        var category = _dbContext.Category.FirstOrDefault(c => c.Id == id);
+        return Ok(category);
+    }
 
+    ///Post Endpoints
+    //Post new category
+    [HttpPost]
+    [Authorize(Roles = "Admin")]
+    public IActionResult CreateCategory(Category category)
+    {
+        _dbContext.Category.Add(category);
+        _dbContext.SaveChanges();
+        return Ok(category);
+    }
 
     ////Put Endpoints
+    [HttpPut("{id}")]
+    [Authorize(Roles = "Admin")]
+    public IActionResult EditCategory(int id, Category category)
+    {
+        Category foundCategory = _dbContext.Category.FirstOrDefault(c => c.Id == id);
+        foundCategory.Name = category.Name;
+        _dbContext.SaveChanges();
+
+        return NoContent();
+
+    }
+
+    ////Delete Endpoints
+    //Delete Category
+    [HttpDelete("{id}")]
+    [Authorize(Roles = "Admin")]
+    public IActionResult DeleteCategory(int id)
+    {
+        Category foundCategory = _dbContext.Category.FirstOrDefault(c => c.Id == id);
+        _dbContext.Category.Remove(foundCategory);
+        _dbContext.SaveChanges();
+
+        return NoContent();
+
+    }
 
 }
 
---
+++ Models/DTO's/CategoryDTO.cs
@@ -0,0 +1,7 @@
+namespace Tabloid.Models.DTOs;
+
+public class CategoryDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
